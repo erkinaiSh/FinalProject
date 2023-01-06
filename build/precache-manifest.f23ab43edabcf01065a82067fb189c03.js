0fe2be25f7b1996695c96e24f413a620c319bc29
erkinaiSh <58220160+erkinaiSh@users.noreply.github.com>
2023-01-06 14:35:00 +0600

    Add files via upload

---
+++ build/precache-manifest.f23ab43edabcf01065a82067fb189c03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317ab4442cafb4664a2f41e86aa7eb32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e034dab5e3defba102",
+    "url": "/static/css/main.a729f1f9.chunk.css"
+  },
+  {
+    "revision": "884e46095daf4b05e386",
+    "url": "/static/js/2.c77075c2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c77075c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e034dab5e3defba102",
+    "url": "/static/js/main.efd25550.chunk.js"
+  },
+  {
+    "revision": "1b193c212e4cdcc9a9c6",
+    "url": "/static/js/runtime-main.46ade884.js"
+  },
+  {
+    "revision": "6a88f712a7d24c51cf3c4f59b75f53da",
+    "url": "/static/media/0be9b123-d00bc462.6a88f712.mp4"
+  },
+  {
+    "revision": "ace703dab1a6049bf729c820a705ad60",
+    "url": "/static/media/Citadel-Rasnov-Romania.ace703da.jpg"
+  },
+  {
+    "revision": "594eda1316157efb2d7cc77b06696954",
+    "url": "/static/media/romania1.594eda13.jpg"
+  },
+  {
+    "revision": "2253011c785f68502d215c36f0d58a1a",
+    "url": "/static/media/romania2.2253011c.jpg"
+  },
+  {
+    "revision": "82bacadf513a1be8d1c0e396047f22ec",
+    "url": "/static/media/romania3.82bacadf.jpg"
+  },
+  {
+    "revision": "0959bd515be0ba3830d7f0cf7f64d53e",
+    "url": "/static/media/romania_karta.0959bd51.gif"
+  }
+]);
